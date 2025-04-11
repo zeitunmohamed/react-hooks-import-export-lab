aa7e7691d24918c85ec5d769d580ff2aab2e68be
zeitunmohamed <zeitunmohamed@gmail.com>
2025-04-11 20:38:47 +0300

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { username, city } from "../data/user";
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>{username} is a Web Developer from {city}</h1>
     </div>
   );
 }
+
+export default Home;
